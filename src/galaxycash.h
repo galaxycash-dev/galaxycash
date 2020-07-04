19c97db8d180a77a4fbd67a0e6eb1d20acd615e6
GalaxyCash <galaxycashen@gmail.com>
2020-07-05 00:00:45 +0300

    v4.1

--- src/galaxycash.h
+++ src/galaxycash.h
@@ -37,7 +37,7 @@ class GalaxyCashToken {
 public:
     std::string name, symbol;
     int64_t supply;
-
+    bool minable;
 
     ADD_SERIALIZE_METHODS;
 
@@ -49,14 +49,15 @@ public:
         if (!(s.GetType() & SER_GETHASH)) READWRITE(supply);
     }
 
-    GalaxyCashToken() : supply(0) { }
-    GalaxyCashToken(const GalaxyCashToken& token) : name(token.name), symbol(token.symbol), supply(token.supply) {}
+    GalaxyCashToken() : supply(0), minable(false) { }
+    GalaxyCashToken(const GalaxyCashToken& token) : name(token.name), symbol(token.symbol), supply(token.supply), minable(token.minable) {}
 
     GalaxyCashToken& operator=(const GalaxyCashToken& token)
     {
         name = token.name;
         symbol = token.symbol;
         supply = token.supply;
+        minable = token.minable;
         return *this;
     }
 
@@ -75,6 +76,32 @@ public:
     uint256 GetHash() const { return SerializeHash(*this); }
 };
 
+class GalaxyCashPoWWork {
+public:
+    uint256 hashPrevBlock;
+    uint32_t nBits, nNonce;
+    int64_t nValue;
+    std::vector<unsigned char> vchWorkSig;
+
+    GalaxyCashPoWWork() : nBits(0), nNonce(0), nValue(0) {}
+    GalaxyCashPoWWork(const GalaxyCashPoWWork &work) : hashPrevBlock(work.hashPrevWork), nBits(work.nBits), nNonce(work.nNonce), nValue(work.nValue), vchWorkSig(work.vchWorkSig) {}
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action)
+    {
+        READWRITE(hashPrevBlock);
+        READWRITE(nBits);
+        READWRITE(nNonce);
+        READWRITE(nValue);
+        if (!(s.GetType() & SER_GETHASH)) READWRITE(vchWorkSig);
+    }
+
+    uint256 GetPoWHash() const { return HashX12(BEGIN(hashPrevBlock), END(nValue)); }
+    uint256 GetHash() const { return SerializeHash(*this); }
+};
+
 class GalaxyCashDB : public CDBWrapper
 {
 public:
@@ -88,6 +115,17 @@ public:
     bool HaveToken(const uint256 &hash);
     bool AccessTokenByName(const std::string& name, GalaxyCashToken& token);
     bool AccessTokenBySymbol(const std::string& symbol, GalaxyCashToken& token);
+
+    bool SetPoWWorkPrevDiff(const uint256 &hash, const uint32_t nBits);
+    bool GetPoWWorkPrevDiff(const uint256 &hash, uint32_t &nBits);
+    bool SetPoWWorkLastDiff(const uint256 &hash, const uint32_t nBits);
+    bool GetPoWWorkLastDiff(const uint256 &hash, uint32_t &nBits);
+    bool GetNextPoWWorkDiff(const uint256 &hash, uint32_t &nBits);
+
+    bool SetPrevPoWWork(const uint256 &hash, const GalaxyCashPoWWork &work);
+    bool GetPrevPoWWork(cosnt uint256 &hash, GalaxyCashPoWWork &work);
+    bool SetLastPoWWork(const uint256 &hash, const GalaxyCashPoWWork &work);
+    bool GetLastPoWWork(cosnt uint256 &hash, GalaxyCashPoWWork &work);
 };
 
 extern std::unique_ptr<GalaxyCashDB> pgdb;
